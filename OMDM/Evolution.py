61c091f0d159943b45ab3c0b6f4bd009d23adcee
Stormjotne <s310455@stud.hioa.no>
2021-09-15 14:06:35 +0200

    Created a placeholder Evolutionary Algorithm that tries to homogenize a genome by punishing distance from mean value and amount of variance. Moved the main file because I had some trouble with import statements when running from outside the package.

--- OMDM/Evolution.py
+++ OMDM/Evolution.py
@@ -1,4 +1,12 @@
-import Fitness, Population
+from multiprocessing import Pool, current_process
+from time import sleep
+from random import random, choice, shuffle
+
+from Population import Population
+from Individual import Individual
+from Model import Model
+import Data
+import Fitness
 
 
 class Evolution:
@@ -6,35 +14,143 @@ class Evolution:
     Implement the Evolutionary Algorithm in this class.
     """
 
-    def __init__(self):
+    def __init__(self, hyper_parameters):
         """
         Put any declarations of object fields/variables in this method.
+        :param hyper_parameters: Dict
         """
-        pass
+        self.number_of_generations = hyper_parameters["number_of_generations"]
+        self.genome_length = hyper_parameters["genome_length"]
+        self.mutation_probability = hyper_parameters["mutation_probability"]
+        self.do_crossover = hyper_parameters["do_crossover"]
+        self.population_size = hyper_parameters["population_size"]
+        self.surviving_individuals = hyper_parameters["surviving_individuals"]
+        self.number_of_parents = hyper_parameters["number_of_parents"]
+        self.best_individual = None
+        self.generation = 0
+        #   Initialize population
+        self.population = Population(self.population_size, self.genome_length)
 
-    def evolve(self):
+    def print_population(self):
+        """
+        Print the current generation number.
+        Print the individual objects and their fitness score.
+        """
+        print("Generation number {}.\n".format(self.generation))
+        for individ in self.population.individuals:
+            print("Individual object: {}\nFitness: {}".format(individ, individ.fitness))
+        print("\n")
+        sleep(1)
+
+    @staticmethod
+    def incubate(individual):
         """
         Generate a phenotype to be evaluated.
-        :return:
+        :param individual:
+        :return: Individual's Phenotype
         """
-        pass
-
-    def reproduce(self, parents, individuals):
+        #   Fake phenotype
+        #   Just sorted genome
+        phenotype = sorted(individual.genotype)
+        return phenotype
+    
+    @staticmethod
+    def evaluate(individual):
         """
-        Generate offspring and/or random mutations.
-        :param parents:
-        :param individuals:
-        :return:
+        Evalute the fitness of an individual in its given environment.
+        :param individual:
+        :return: Individual's Fitness
         """
-        pass
+        #   This is just placeholder stuff
+        #   Send the phenotype to a function that calculates its fitness.
+        fitness = Fitness.placeholder_fitness(individual.phenotype)
+        return fitness
 
     def select_parents(self, individuals):
         """
         Select a number of best individuals to serve as the basis for the next generation.
         :param individuals:
-        :return:
+        :return: The best Individuals
         """
-        pass
+        individuals.sort(key=lambda element: element.fitness, reverse=True)
+        best_individuals = individuals[:self.number_of_parents]
+        return best_individuals
+    
+    def reproduce(self, parents, individuals):
+        """
+        Generate offspring and/or random mutations.
+        :param parents:
+        :param individuals:
+        :return: The Next Generation
+        """
+        individuals.sort(key=lambda element: element.fitness, reverse=True)
+        #   Individuals surviving to the next generation.
+        retained_adults = individuals[:self.surviving_individuals]
+        next_generation = retained_adults if retained_adults else []
+        i = 0
+        if self.do_crossover:
+            while len(next_generation) < self.population_size:
+                new_genome = self.crossover(parents[i].genotype, parents[i + 1].genotype)
+                next_generation.append(Individual(genotype=new_genome))
+                i = (i + 2) % (len(parents) - 1)
+        else:
+            while len(next_generation) < self.population_size:
+                new_genome = self.clone(parents[i % self.number_of_parents].genotype)
+                next_generation.append(Individual(genotype=new_genome))
+                i += 1
+        return next_generation
+    
+    def clone(self, parent_genome):
+        """
+        Create new genome from parent's genome with a chance of mutation.
+        :param parent_genome:
+        """
+        genome = []
+        for gene in parent_genome:
+            genome.append(random() if random() < self.mutation_probability else gene)
+        return genome
+
+    def crossover(self, parent_genome_one, parent_genome_two):
+        """
+        Create new genome from two parents' genomes with a chance of mutation.
+        :param parent_genome_one:
+        :param parent_genome_two:
+        """
+        genome = []
+        for gene1, gene2 in zip(parent_genome_one, parent_genome_two):
+            genome.append(random() if random() < self.mutation_probability else choice((gene1, gene2)))
+        return genome
+    
+    def evolve(self, printout=False):
+        """
+        Run the algorithm for a set number of generations.
+        Generate a phenotype from the genotype.
+        Evaluate the fitness of the phenotype.
+        Select parents for the next generation.
+        Select the best individual of the generation.
+        Populate the next generation by reproduction.
+        """
+        if printout:
+            print("The goal of this EA is to converge the mean value in the genome to a certain target value,"
+                  " with as little variance as possible.")
+            sleep(5)
+            print("You'll see at the end that the phenotype will be a sorted list"
+                  " where the mean value is very close to 0.5 with (hopefully) very little variance.")
+            sleep(5)
+            print("Starting evolution.")
+            sleep(2)
+        while self.generation < self.number_of_generations:
+            for individual in self.population.individuals:
+                individual.phenotype = self.incubate(individual)
+                individual.fitness = self.evaluate(individual)
+            #   Print all individuals and their fitness
+            if printout:
+                self.print_population()
+            parents = self.select_parents(self.population.individuals)
+            self.best_individual = self.population.individuals[0]
+            self.population.individuals = self.reproduce(parents, self.population.individuals)
+            self.generation += 1
+        return self.best_individual
 
 
 #   Use this conditional to test the class by running it "standalone".
