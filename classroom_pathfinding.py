75308207eecb0b7a3dbf9f6382f1e3fcd22b913f
Vuhit <37663698+Vuhit@users.noreply.github.com>
2021-09-26 23:43:50 +0200

    Added 3 new files. The file: pathfinding - is the implementation of the pathfinding algorithm. The file classroom_pathfinding is the classroom layout with pathfinding implemented. The file: constants includes the world size constant needed by both the other files.

--- classroom_pathfinding.py
+++ classroom_pathfinding.py
@@ -1,12 +1,17 @@
+import random
+
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import time
+from pathfinding import make_grid, find_path
+from constants import world_size
 
 # world size, 300 for single class room view, 500/1000 for more building space
-world_size = 300
 world = np.zeros((world_size, world_size, 3))
 world_map = np.zeros((world_size, world_size))
+grid = make_grid(world_size)
+
 
 # world borders:
 
@@ -68,13 +73,19 @@ world_map[200:225, 250:254] = 0
 
 D3_world_map = np.repeat(world_map[:, :, np.newaxis], 3, axis=2)
 
-nr_of_agents = 100
+
+nr_of_agents = 12  # current max 42
+
 agent_list_infected = np.random.rand(nr_of_agents) > 1
 agent_list_infected[0] = 1
 agent_list_susceptible = np.ones(nr_of_agents)
 
 
 positions = np.random.rand(2, nr_of_agents)*world_size
+
+
+
+
 infected_positions = np.where(agent_list_infected == 1)
 
 velocity = (np.random.rand(2, nr_of_agents)-0.5)*2
@@ -94,58 +105,96 @@ nr_of_infected = []
 plt.title("Current positions")
 plt.xlabel("x positions")
 plt.ylabel("y positions")
+
+destinations = [[120, 120, 120, 120, 120, 120, 145, 145, 145, 145, 145, 145, 145, 145, 145, 170, 170, 170, 170, 170, 170
+                    , 170, 170, 170, 205, 205, 205, 205, 205, 205, 205, 205, 205, 230, 230, 230, 230, 230, 230, 230, 230, 230],
+                [85, 95, 145, 155, 205, 215, 80, 90, 100, 140, 150, 160, 200, 210, 220, 80, 90, 100, 140, 150, 160, 200,
+                 210, 220, 80, 90, 100, 140, 150, 160, 200, 210, 220, 80, 90, 100, 140, 150, 160, 200, 210, 220]]
+
+path = []
+
+def calculate_path(nr_of_agents,grid,world_map,world_size,positions,destinations):
+    p = []
+
+    for i in range(nr_of_agents):
+        p.append([])
+        p[i] = find_path(grid, world_map, world_size, positions[0][i], positions[1][i], destinations[0][i], destinations[1][i])
+    return p
+
+random_movement = 1
+count = 0
 while True:
-    p_1 = np.repeat(positions[:, :, np.newaxis], positions.shape[1], axis=2)
-    p_2 = np.rot90(p_1, axes=(1, 2))
-    p_1 -= p_2
+    #random movement:
+    if random_movement == 1:
 
-    distances = np.linalg.norm(p_1, axis=0)
-    distances[np.arange(nr_of_agents), np.arange(nr_of_agents)] = infection_range + 20
 
-    infection_cases = np.array(np.where(distances < infection_range))
-    velocity_cases = np.array(np.where(distances < velocity_range))
-    attraction_cases = np.array(np.where(distances < attraction_range))
-    dispersion_cases = np.array(np.where(distances < dispersion_range))
+            p_1 = np.repeat(positions[:, :, np.newaxis], positions.shape[1], axis=2)
+            p_2 = np.rot90(p_1, axes=(1, 2))
+            p_1 -= p_2
+
+            distances = np.linalg.norm(p_1, axis=0)
+            distances[np.arange(nr_of_agents), np.arange(nr_of_agents)] = infection_range + 20
+
+            infection_cases = np.array(np.where(distances < infection_range))
+            velocity_cases = np.array(np.where(distances < velocity_range))
+            attraction_cases = np.array(np.where(distances < attraction_range))
+            dispersion_cases = np.array(np.where(distances < dispersion_range))
     # print(distances)
     # print()
 
     # print(infection_cases.shape)
 
-    if infection_cases.shape[1] >= 1:
-        infections = agent_list_infected[infection_cases[0, :]] == agent_list_susceptible[infection_cases[1, :]]
-        infections_where = np.array(np.where(infections == 1))
+            if infection_cases.shape[1] >= 1:
+                infections = agent_list_infected[infection_cases[0, :]] == agent_list_susceptible[infection_cases[1, :]]
+                infections_where = np.array(np.where(infections == 1))
 
-        agent_list_infected[infection_cases[1, infections_where]] = 1
-        agent_list_susceptible[infection_cases[1, infections_where]] = 0
-    if dispersion_cases.shape[1] >= 1:
+                agent_list_infected[infection_cases[1, infections_where]] = 1
+                agent_list_susceptible[infection_cases[1, infections_where]] = 0
+            if dispersion_cases.shape[1] >= 1:
         # This would be where you implement social distancing as a force moving agent apart.
         # This is done in BOID simulations so look into that.
-        pass
-    if attraction_cases.shape[1] >= 1:
+                pass
+            if attraction_cases.shape[1] >= 1:
         # make agents cluster - again BOIDS
-        pass
-    if velocity_cases.shape[1] >= 1:
+                pass
+            if velocity_cases.shape[1] >= 1:
         # make agents align their movement - BOIDS
-        pass
+                pass
 
         # wall interaction
-    for i0 in range(nr_of_agents):
-        wall_perception = world_map[
+            for i0 in range(nr_of_agents):
+                wall_perception = world_map[
                       (positions[0, i0] - max_speed).astype(np.int32):(positions[0, i0] + max_speed + 1).astype(
                           np.int32),
                       (positions[1, i0] - max_speed).astype(np.int32):(positions[1, i0] + max_speed + 1).astype(
                           np.int32)]
-        wall_location = np.array(np.where(wall_perception > 0))
+                wall_location = np.array(np.where(wall_perception > 0))
     # subtract max speed to make the values relative to the center
-        wall_location -= max_speed
+                wall_location -= max_speed
 
-        velocity[:, i0] -= np.sum(wall_location, 1) * 10
+                velocity[:, i0] -= np.sum(wall_location, 1) * 10
 
-    velocity += (np.random.rand(2, nr_of_agents) - 0.5) * 2
-    velocity_length[:] = np.linalg.norm(velocity, ord=2, axis=0)
-    velocity *= max_speed / velocity_length
+            velocity += (np.random.rand(2, nr_of_agents) - 0.5) * 2
+            velocity_length[:] = np.linalg.norm(velocity, ord=2, axis=0)
+            velocity *= max_speed / velocity_length
 
-    positions += velocity
+            positions += velocity
+            count += 1
+            if count == 80:
+                random_movement = 0
+
+    if random_movement == 0:
+        if not path:
+            path = calculate_path(nr_of_agents,grid,world_map,world_size,positions,destinations)
+        for i in range(nr_of_agents):
+
+            if path[i]:
+                x1 = path[i][0][1]
+                y1 = path[i][0][0]
+                positions[0][i] = y1
+                positions[1][i] = x1
+                path[i].pop(0)
+
 
     # here dealing with agents moving outside the world
     above_world_size = np.array(np.where(positions > world_size))
---
+++ pathfinding.py
@@ -0,0 +1,185 @@
+import numpy as np
+from queue import PriorityQueue
+from constants import world_size
+
+WALL = 'wall'
+EMPTY = 'empty'
+
+#start_cor_y = 240
+#start_cor_x = 150
+
+#end_cor_y = 120
+#end_cor_x = 145
+
+
+
+class Cell:
+    def __init__(self, row, col):
+        self.col = col
+        self.row = row
+        self.state = EMPTY
+        self.neighbours = []
+        self.total_size = world_size
+        self.total_rows = world_size
+
+    # returns the x and y coordinate of the cell.
+    def get_pos(self):
+        return self.row, self.col
+
+    # returns true if the cell contains a wall.
+    def is_wall(self):
+        return self.state == WALL
+
+    # sets the cell to contain a wall
+    def make_wall(self):
+        self.state = WALL
+
+    # this function goes through the whole grid of cell objects and
+    # finds out if their direct neighbour cells contain walls. Adds the non-wall neighbours to a list.
+    def find_neighbours(self, grid):
+        self.neighbours = []
+        if self.row < self.total_size -1 and not grid[self.row+1][self.col].is_wall():  # down neighbour
+            self.neighbours.append(grid[self.row+1][self.col])
+
+        if self.row > 0 and not grid[self.row-1][self.col].is_wall():  # up neighbour
+            self.neighbours.append(grid[self.row-1][self.col])
+
+        if self.col < self.total_rows -1 and not grid[self.row][self.col + 1].is_wall():  # right neighbour
+            self.neighbours.append(grid[self.row][self.col + 1 ])
+
+        if self.col > 0 and not grid[self.row][self.col-1].is_wall():  # left neighbour
+            self.neighbours.append(grid[self.row][self.col-1])
+
+
+        # additional 4 directions
+        if (self.row < self.total_size -1 and self.col > 0) and not grid[self.row+1][self.col-1].is_wall():  # down left neighbour
+            self.neighbours.append(grid[self.row+1][self.col-1])
+
+        if (self.row < self.total_size-1 and self.col < self.total_rows-1) and not grid[self.row+1][self.col+1].is_wall():  # down right neighbour
+            self.neighbours.append(grid[self.row+1][self.col+1])
+
+        if (self.row > 0 and self.col > 0) and not grid[self.row-1][self.col - 1].is_wall():  # upper left neighbour
+            self.neighbours.append(grid[self.row-1][self.col-1])
+
+        if (self.row > 0 and self.col < self.total_rows-1) and not grid[self.row-1][self.col+1].is_wall():  # upper right neighbour
+            self.neighbours.append(grid[self.row-1][self.col+1])
+
+        pass
+
+
+# this function creates a grid of cell objects
+def make_grid(world_size):
+
+    grid = []
+
+    for i in range(world_size):
+        grid.append([])
+        for j in range(world_size):
+            cell = Cell(i, j)
+            grid[i].append(cell)
+
+    return grid
+
+
+# this function looks for walls on the world map and places them in the correct cells on the cell grid.
+def find_walls(grid, world_map, world_size):
+
+    for i in range(world_size):
+        for j in range(world_size):
+            if world_map[i][j] == 20:
+                cell = grid[i][j]
+                cell.make_wall()
+
+    return
+
+
+# this function calculetes the heuristic score,
+# which is the direct distance between the current cell and the goal.
+def h(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+# this function recreates the shortest path from the end cell to the start cell and puts it in a list.
+def reconstruct_path(came_from, current):
+
+    path = []
+    path.append(current.get_pos())
+    while current in came_from:
+        current = came_from[current]
+        path.append(current.get_pos())
+
+    return path
+
+
+# this is the pathfinding algorithm itself
+def pathfinding(grid, start, end_goal):
+
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    came_from = {}
+    # here we set the initial g- and f-score to be infinate,
+    # any value that we actually calculate will be lower and replace the infinite one.
+    g_score = {cell: float("inf") for row in grid for cell in row}
+    g_score[start] = 0
+    f_score = {cell: float("inf") for row in grid for cell in row}
+    f_score[start] = h(start.get_pos(), end_goal.get_pos())
+
+    open_set_hash = {start}
+
+    while not open_set.empty():
+
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+        if current == end_goal:
+
+            path = reconstruct_path(came_from, end_goal)
+            return path
+
+        for neighbour in current.neighbours:
+            temp_g_score = g_score[current] + 1  # neighbour at least 1 hop away
+
+            if temp_g_score < g_score[neighbour]:
+                came_from[neighbour] = current  # 1h21m
+                g_score[neighbour] = temp_g_score
+                f_score[neighbour] = temp_g_score + h(neighbour.get_pos(), end_goal.get_pos())
+                if neighbour not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbour], count, neighbour))
+                    open_set_hash.add(neighbour)
+
+    return False
+
+#def find_path(grid,world_map,world_size, start_cor, end_cor):
+def find_path(grid, world_map, world_size, start_cor_y, start_cor_x, end_cor_y, end_cor_x):
+
+    start_cor_y = start_cor_y.astype(np.int32)
+    start_cor_x = start_cor_x.astype(np.int32)
+
+    end_cor_y = end_cor_y
+    end_cor_x = end_cor_x
+
+    #print (end_cor_y,end_cor_x)
+    find_walls(grid, world_map, world_size)
+
+    cell = grid[start_cor_y][start_cor_x]
+    start = cell
+
+    cell = grid[end_cor_y][end_cor_x]
+    end_goal = cell
+
+    for row in grid:
+        for cell in row:
+            cell.find_neighbours(grid)
+
+    p = pathfinding(grid, start, end_goal)
+
+    #print(p)
+    p.reverse()
+    #print(p)
+
+    return p
+
+
