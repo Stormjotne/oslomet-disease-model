c650e36a9a08cb4ca908eaf7b32edd2f855cd96a
Stormjotne <s310455@stud.hioa.no>
2021-10-12 16:08:30 +0200

    Added multiprocessing to the steps where individual phenotype (abm model output) and fitness score are generated.

--- OMDM/Evolution.py
+++ OMDM/Evolution.py
@@ -1,6 +1,7 @@
 """
 
 """
+import os
 from multiprocessing import Pool, current_process
 from time import sleep
 from random import random, choice, shuffle
@@ -16,11 +17,13 @@ class Evolution:
     Implement the Evolutionary Algorithm in this class.
     """
 
-    def __init__(self, hyper_parameters):
+    def __init__(self, hyper_parameters, printout=False):
         """
         Put any declarations of object fields/variables in this method.
         :param hyper_parameters: Dict
         """
+        self.printout = printout
+        self.number_of_threads = os.cpu_count() - 1
         self.number_of_generations = hyper_parameters["number_of_generations"]
         self.genome_length = hyper_parameters["genome_length"]
         self.mutation_probability = hyper_parameters["mutation_probability"]
@@ -36,11 +39,10 @@ class Evolution:
 
     def print_population(self):
         """
-        Print the current generation number.
         Print the individual objects and their fitness score.
+        Only prints the first individual in the sorted list for now.
         """
-        print("Generation number {}.\n".format(self.generation))
-        print("Generation Best: {}".format(self.population.individuals[0].fitness))
+        print("\nGeneration Best: {}".format(self.population.individuals[0].fitness))
         #   for individ in self.population.individuals:
         #       print("Individual object: {}\nFitness: {}".format(individ, individ.fitness))
         print("\n")
@@ -64,7 +66,7 @@ class Evolution:
         :param individual:
         :return: Individual's Phenotype
         """
-        new_model = Model(individual.genome, normalized=True, static_population=1000)
+        new_model = Model(individual.genome.genome, normalized=True, static_population=1000)
         phenotype = new_model.placeholder_simulate()
         return phenotype
     
@@ -75,10 +77,23 @@ class Evolution:
         :param individual:
         :return: Individual's Fitness
         """
-        #   This is just placeholder stuff
         #   Send the phenotype to a function that calculates its fitness.
         fitness = Fitness.relative_spread_fitness(individual.phenotype)
         return fitness
+    
+    def generate(self, individual):
+        """
+        Generate the phenotype and fitness score of an individual.
+        :param individual:
+        :type individual:
+        :return:
+        :rtype:
+        """
+        if self.printout:
+            print(current_process().name, end=" ")
+        individual.phenotype = self.incubate(individual)
+        individual.fitness = self.evaluate(individual)
+        return individual
 
     def select_parents(self):
         """
@@ -88,8 +103,8 @@ class Evolution:
         self.population.individuals.sort(key=lambda element: element.fitness, reverse=True)
         best_individuals = self.population.individuals[:self.number_of_parents]
         return best_individuals
-    
-    def evolve(self, printout=False):
+        
+    def evolve(self):
         """
         Run the algorithm for a set number of generations.
         Generate a phenotype from the genotype.
@@ -98,18 +113,29 @@ class Evolution:
         Select the best individual of the generation.
         Populate the next generation by reproduction.
         """
-        if printout:
+        if self.printout:
             print("Starting evolution.")
-            sleep(2)
+            sleep(1)
+            print("Found {} available CPU Cores/Threads.".format(self.number_of_threads))
+            sleep(1)
         while self.generation < self.number_of_generations:
             #   Wrap the next loop in threads.
-            for individual in self.population.individuals:
-                #   Create a phenotype from the individual's genotype.
-                individual.phenotype = self.incubate(individual.genome)
-                #   Evaluate the fitness of the individual.
-                individual.fitness = self.evaluate(individual)
+            '''for individual in self.population.individuals:
+                    #   Create a phenotype from the individual's genotype.
+                    individual.phenotype = self.incubate(individual.genome)
+                    #   Evaluate the fitness of the individual.
+                    individual.fitness = self.evaluate(individual)'''
+            #   Use multiprocessing.Pool to generate phenotype and fitness score in parallel.
+            with Pool(self.number_of_threads) as p:
+                if self.printout:
+                    print("\nGeneration number {}.".format(self.generation))
+                    print(current_process(), end=" ")
+                #   Straight up overwrite population with updated individuals
+                #   Maybe there's a nicer way
+                self.population.individuals = p.map(self.generate, self.population.individuals)
+                p.close()
             #   Do generational printout.
-            if printout:
+            if self.printout:
                 self.print_population()
             #   Select the parents of the next generations.
             #   This method also currently sorts the population by fitness.
--- main.py
+++ main.py
@@ -8,22 +8,27 @@ Run the model optimization program we're creating from this script.
 """
 
 hyper_parameters = {
-    "number_of_generations": 100,
+    "number_of_generations": 10,
     "genome_length": 9,
     "mutation_probability": 0.2,
     "do_crossover": True,
-    "population_size": 10,
-    "surviving_individuals": 2,
-    "number_of_parents": 2
+    "population_size": 60,
+    "surviving_individuals": 10,
+    "number_of_parents": 10
 }
 
 #   Code after this conditional is only executes if the python process originates from this script.
 if __name__ == "__main__":
-    Evo = Evolution.Evolution(hyper_parameters)
-    #   Run evolution. Printout flag is False by default, so just run the method with no arguments to omit prints.
-    result = Evo.evolve(printout=True)
+    Evo = Evolution.Evolution(hyper_parameters, printout=True)
+    #   Run evolution.
+    result = Evo.evolve()
     print("The evolution is complete.\nThe best individual ended up with the following properties:\n")
-    print("Fitness: {}\nGenotype: {}\nPhenotype: {}".format(result.fitness, result.genome.genome, result.phenotype))
+    sleep(1)
+    print("Fitness: {}".format(result.fitness))
+    sleep(1)
+    print("Genotype: {}".format(result.genome.genome))
+    sleep(1)
+    print("Phenotype: {}".format(result.phenotype))
     '''
     print("\nTry running it again with an increased number of generations, but remove the printout flag in line 23.")
     '''
